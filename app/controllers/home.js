5a103ebd2f2f11a0c8803788063951bcbe0b2455
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-01 15:44:51 +0100

    Volviendo a configurar el proyecto DapsServer

---
+++ app/controllers/home.js
@@ -0,0 +1,4 @@
+
+exports.home = function(req, res){
+  res.render('index', {title: 'DAPS: Doctors As your PartnerS'});
+};
---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'dapsserver'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/dapsserver-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'dapsserver'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/dapsserver-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'dapsserver'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/dapsserver-production'
+  }
+};
+
+module.exports = config[env];
