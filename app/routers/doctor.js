5b28a2c22275eead1db74856003de5968b1ab39f
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-02 13:24:47 +0100

    añadida la ruta POST: /doctor/new

--- app/routers/doctor.js
+++ app/routers/doctor.js
@@ -2,7 +2,9 @@ var express = require('express');
 var doctorRouter = express.Router();
 var doctorController = require('../controllers/doctor');
 
-//  GET '/'
+//  GET '/new'
 doctorRouter.get('/new', doctorController.new);
+// POST '/new'
+doctorRouter.post('/new', doctorController.create);
 
 module.exports = doctorRouter;
