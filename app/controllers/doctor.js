b278eea04f3c2527bb271140c8000db54f11c065
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-02 13:16:13 +0100

    Intentando enviar una variable doctor en el controllers/doctor/new

--- app/controllers/doctor.js
+++ app/controllers/doctor.js
@@ -5,6 +5,15 @@ exports.new = function (req, res){
   var errors = req.session.errors || {};
   req.session.errors = {};
 
+  var doctor = new Doctor({
+    username: "",
+    password: "",
+    name_and_surname: "",
+    colegiado: 0,
+    especialidad: "",
+    email: "",
+    mobile: 0
+  });
 
-  res.render('pages/doctor/new', {errors: errors});
+  res.render('pages/doctor/new', {docto: doctor, errors: errors});
 };
