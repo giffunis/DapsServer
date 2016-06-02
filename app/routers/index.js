a612381d49bd77944a2e8b84489f8b10ed7336b4
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-02 10:17:57 +0100

    el login lo llevará u session Controller específico

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var homeController = require('../controllers/home');
+var sessionController = require('../controllers/session');
 
 //  GET '/'
 router.get('/', homeController.home);
 // login GET
-router.get('/login', homeController.login);
+router.get('/login', sessionController.login);
 
 module.exports = router;
