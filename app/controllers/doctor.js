156336f821526ee8188c03968a68da38794ad0d9
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-02 13:11:35 +0100

    Añadidas las variables para usar DoctorSchema en el controllers/doctor

--- app/controllers/doctor.js
+++ app/controllers/doctor.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 var Doctor = mongoose.model('Doctor');
 
 exports.new = function (req, res){
   var errors = req.session.errors || {};
   req.session.errors = {};
+
+
   res.render('pages/doctor/new', {errors: errors});
 };
