a24a7578ae6a21507a552407d824276bad00854c
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-07 13:16:34 +0100

    añadido el autoload al fichero routers/quiz.js

--- app/routers/quiz.js
+++ app/routers/quiz.js
@@ -3,6 +3,9 @@ var quizRouter = express.Router();
 var quizController = require('../controllers/quiz');
 var sessionController = require('../controllers/session');
 
+// Autoloads
+quizRouter.param('quizId', quizController.load);
+
 //  GET '/'
 // quizRouter.get('/', sessionController.loginRequired, quizController.index);
 quizRouter.get('/', quizController.index);
