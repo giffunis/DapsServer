244fc12d14f180cf7342eba5092346caea2d9ab8
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-07 18:21:33 +0100

    ahora es mejor esa redirección del login

--- app/controllers/session.js
+++ app/controllers/session.js
@@ -24,8 +24,12 @@ exports.create = function (req, res) {
     }
     req.session.user = {id: user.id, username: user.username};
     console.log("el usuario se ha validado correctamente");
-    res.redirect(req.session.redir.toString());
-    // res.redirect('/doctor/');
+    if(req.session.redir.toString() !='/'){
+      res.redirect(req.session.redir.toString());
+    } else {
+        res.redirect('/doctor/');
+    }
+
   });
 };
 
