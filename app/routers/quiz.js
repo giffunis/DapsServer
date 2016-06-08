46dc463f4c38e47f11764dcda6ea208be2889d97
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-08 11:25:06 +0100

    añadida una nueva ruta para la subida

--- app/routers/quiz.js
+++ app/routers/quiz.js
@@ -10,4 +10,6 @@ quizRouter.param('quizId', quizController.load);
 quizRouter.get('/', sessionController.loginRequired, quizController.index);
 // GET '/:quizId'
 quizRouter.get('/:quizId([a-z0-9]{24})', sessionController.loginRequired, quizController.show);
+// Get '/upload'
+quizRouter.get('/upload', sessionController.loginRequired, quizController.newUpload);
 module.exports = quizRouter;
