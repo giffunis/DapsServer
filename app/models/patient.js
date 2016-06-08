d78b52f9a03a946dfa10d51cb22c5da3e53077d4
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-08 18:32:45 +0100

    Creado el schema de los pacientes

--- app/models/patient.js
+++ app/models/patient.js
@@ -5,7 +5,60 @@ var uniqueValidator = require('mongoose-unique-validator');
 
 
 var PatientSchema = new Schema({
-
+  name: {
+    type: String,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  dni: {
+    type: String,
+    required: [true, 'El {PATH} es obligatorio'],
+    unique: true
+  },
+  sex: {
+    type: String,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  birthDate: {
+    type: Date,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  mobileNumber: {
+    type: String,
+    validate: {
+      validator: function(phone){
+        return /\d{3}-\d{2}-\d{2}-\d{2}/.test(phone);
+      },
+      message:'{VALUE} No es un número válido, debe seguir la estructura: XXX-XX-XX-XX'
+    }
+  },
+  contactPerson: {
+    type: String
+  },
+  personTlf: {
+    type: String,
+    validate: {
+      validator: function(phone){
+        return /\d{3}-\d{2}-\d{2}-\d{2}/.test(phone);
+      },
+      message:'{VALUE} No es un número válido, debe seguir la estructura: XXX-XX-XX-XX'
+    }
+  },
+  smoker: {
+    type: Boolean,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  memoryProblems: {
+    type: Boolean,
+    required: [true, 'El {PATH} es obligatorio']
+  },
+  heartCondition: {
+    type: Boolean,
+    required: [true, 'El {PATH} es obligatorio']
+  },
 });
 
 PatientSchema.plugin(uniqueValidator, {message: 'El {PATH} ya se encuentra en uso'});
