5b15e2271b834ff19b86ef1fafa2f4060dff4cb8
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-09 12:37:46 +0100

    añadida una nueva ruta

--- app/routers/patient.js
+++ app/routers/patient.js
@@ -7,6 +7,7 @@ var sessionController = require('../controllers/session');
 //  GET '/new'
 patientRouter.get('/new', sessionController.loginRequired, patientController.new);
 patientRouter.post('/new', sessionController.loginRequired, patientController.create);
+patientRouter.get('/', sessionController.loginRequired, patientController.index);
 
 
 module.exports = patientRouter;
