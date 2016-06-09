4473db30f07e74948c5fc0ecfa3f56c86d30172a
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-09 14:00:51 +0100

    añadidas dos nuevas líneas en el router de patients

--- app/routers/patient.js
+++ app/routers/patient.js
@@ -3,11 +3,15 @@ var patientRouter = express.Router();
 var patientController = require('../controllers/patient');
 var sessionController = require('../controllers/session');
 
+// Autoloads
+patientRouter.param('patientId', patientController.load);
 
 //  GET '/new'
 patientRouter.get('/new', sessionController.loginRequired, patientController.new);
 patientRouter.post('/new', sessionController.loginRequired, patientController.create);
 patientRouter.get('/', sessionController.loginRequired, patientController.index);
+quizRouter.get('/:patientId([a-z0-9]{24})', sessionController.loginRequired, patientController.show);
+
 
 
 module.exports = patientRouter;
