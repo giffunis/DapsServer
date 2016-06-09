416b3ff00e2b69021166d7b8aded8db7645691f8
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-09 11:06:31 +0100

    añadida la ruta post

--- app/routers/patient.js
+++ app/routers/patient.js
@@ -6,7 +6,7 @@ var sessionController = require('../controllers/session');
 
 //  GET '/new'
 patientRouter.get('/new', sessionController.loginRequired, patientController.new);
-
+patientRouter.post('/new', sessionController.loginRequired, patientController.create);
 
 
 module.exports = patientRouter;
