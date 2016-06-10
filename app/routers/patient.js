9b51b9294e336b7d5e2955536af6d64f974918a4
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-10 12:12:58 +0100

    creada una nueva ruta post para añadir un test al usuario

--- app/routers/patient.js
+++ app/routers/patient.js
@@ -12,7 +12,7 @@ patientRouter.post('/new', sessionController.loginRequired, patientController.cr
 patientRouter.get('/', sessionController.loginRequired, patientController.index);
 patientRouter.get('/:patientId([a-z0-9]{24})', sessionController.loginRequired, patientController.show);
 
-// patientRouter.post('/:patientId([a-z0-9]{24})/quiz/add', sessionController.loginRequired, patientController.quizAdd);
+patientRouter.post('/:patientId([a-z0-9]{24})/quiz/add/:quizId([a-z0-9]{24})', sessionController.loginRequired, patientController.quizAdd);
 
 
 
