2044356f6391934abb92bfa16d8a1c7a902b585e
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-28 19:52:13 +0100

    me traicionó el subcon

--- app/controllers/heartBeat.js
+++ app/controllers/heartBeat.js
@@ -1,10 +1,16 @@
-
-
-
-exports.load = function(req,res,next,heartBeatId){
-  next();
-};
+var mongojs = require('mongojs');
+var heartBeatDB = mongojs('mongodb://localhost/dapsserver-development', ['heartbeats']);
 
 exports.show = function(req,res){
-  res.send("prueba");
+  var id = req.query.search;
+  console.log("Los datos que se buscan son: " + id);
+  heartBeatDB.heartbeats.findOne({ _id: mongojs.ObjectId(id)}, function(err, doc) {
+    if(err){
+      console.log("No se ha encontrado el documento");
+      res.send("Datos no encontrados");
+    }else {
+      console.log("Se ha encontrado el siguiente doc:\n" + doc);
+      res.send(doc);
+    }
+  });
 };
--- app/routers/heartBeat.js
+++ app/routers/heartBeat.js
@@ -3,10 +3,7 @@ var heartBeatRouter = express.Router();
 var sessionController = require('../controllers/session');
 var heartBeatController = require('../controllers/heartBeat');
 
-// Autoloads
-heartBeatRouter.param('heartBeatId', heartBeatController.load);
-
 // Rutas para obtener los datos del corazón
-heartBeatRouter.get('/:heartBeatId', sessionController.loginRequired, heartBeatController.show);
+heartBeatRouter.get('/', sessionController.loginRequired, heartBeatController.show);
 
 module.exports = heartBeatRouter;
