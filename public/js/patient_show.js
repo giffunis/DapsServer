bfbecb7950140ad3d938645269c251236c0e9235
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-30 00:30:59 +0100

    añadida la función para la frecuencia cardiaca

--- public/js/patient_show.js
+++ public/js/patient_show.js
@@ -46,8 +46,54 @@ $(function(){
 
  });
 
+ $('#pulsar2').click(function(){
+
+   var options = {
+    lines: {
+      show: true
+    },
+    points: {
+      show: true
+    },
+    xaxis: {
+      mode: "categories",
+      tickSize: 0
+    }
+  };
+
+  var data = [];
+  $.plot($("#heartbeat-area-chart"), data, options);
+  //  $.plot($("#activityGraphic"), data, options);
+
+  // Fetch one series, adding to what we already have
+  var alreadyFetched = {};
+
+  // Función que recarga el gráfico.
+  function onDataReceived(series) {
+    console.log("function onDataReceived");
+    if (!alreadyFetched[series.label]) {
+      alreadyFetched[series.label] = true;
+      console.log("almacenando las series en data");
+      data.push(series);
+      console.log(JSON.stringify(data[0]));
+    }
+    $.plot($("#heartbeat-area-chart"), data, options);
+  } // onDataReceived()
+
+   var parameters = {
+                      patientId: $("#patientId2").val(),
+                      desde: $("#heartbeat-calendar-desde").val(),
+                      hasta: $("#heartbeat-calendar-hasta").val()
+                    };
+   $.get( '/heartbeat',parameters, function(series) {
+     console.log("Datos obtenidos, llamando a onDataReceived");
+     onDataReceived(series);
+   }); // $.get
+
+ });
+
 
   // callendars
-  $( "#activity-calendar-desde" ).datepicker();
-  $( "#activity-calendar-hasta" ).datepicker();
+  $( "#heartbeat-calendar-desde" ).datepicker();
+  $( "#heartbeat-calendar-hasta" ).datepicker();
 });
