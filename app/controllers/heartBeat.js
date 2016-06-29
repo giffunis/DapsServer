93b167f514462cd7e9659babb5af0eeda7817b79
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-30 00:34:51 +0100

    actualizado el heartbeats

--- app/controllers/heartBeat.js
+++ app/controllers/heartBeat.js
@@ -1,16 +1,26 @@
 var mongojs = require('mongojs');
-var heartBeatDB = mongojs('mongodb://localhost/dapsserver-development', ['heartbeats']);
+var heartbeatsDB = mongojs('mongodb://localhost/dapsserver-development', ['heartbeats']);
 
 exports.show = function(req,res){
-  var id = req.query.search;
-  console.log("Los datos que se buscan son: " + id);
-  heartBeatDB.heartbeats.findOne({ _id: mongojs.ObjectId(id)}, function(err, doc) {
+  var id = req.query.patientId;
+  var desde = new Date(req.query.desde);
+  var hasta = new Date(req.query.hasta);
+  console.log(id);
+  heartbeatsDB.heartbeats.find({ patientId: mongojs.ObjectId(id), created_at: { '$gte': desde, '$lte': hasta}}, function (err, docs) {
     if(err){
-      console.log("No se ha encontrado el documento");
-      res.send("Datos no encontrados");
-    }else {
-      console.log("Se ha encontrado el siguiente doc:\n" + doc);
-      res.send(doc);
+      console.log("Error en la consulta");
+      res.send(err);
+    }else if (docs.length !== 0) {
+      var data = [];
+      var punto;
+      for (var i = 0; i < docs.length; i++) {
+        punto = [docs[i].created_at.getMonth() + "/" + docs[i].created_at.getDay(), docs[i].beats];
+        data.push(punto);
+      }
+      var salida = {"label": "Frecuencia Cardiaca", "data": data};
+      res.send(salida);
+    } else {
+      res.send("No existen datos");
     }
   });
 };
