01a110f02ec1e1d62a1ab91af1131ab469299285
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-06-30 14:01:55 +0100

    activity muestra los datos correctamente

--- app/controllers/activity.js
+++ app/controllers/activity.js
@@ -6,7 +6,7 @@ exports.show = function(req,res){
   var desde = new Date(req.query.desde);
   var hasta = new Date(req.query.hasta);
   console.log(id);
-  activityDB.activity.find({ patientId: mongojs.ObjectId(id), created_at: { '$gte': desde, '$lte': hasta}}, function (err, docs) {
+  activityDB.activity.find({ patientId: mongojs.ObjectId(id), created_at: { '$gte': desde, '$lte': hasta}}).sort({created_at: 1}, function (err, docs) {
     if(err){
       console.log("Error en la consulta");
       res.send(err);
@@ -14,7 +14,10 @@ exports.show = function(req,res){
       var data = [];
       var punto;
       for (var i = 0; i < docs.length; i++) {
-        punto = [docs[i].created_at.getMonth() + "/" + docs[i].created_at.getDay(), docs[i].steps];
+        console.log(docs[i]);
+        var day = docs[i].created_at.getDate();
+        var month = docs[i].created_at.getMonth() + 1;
+        punto = [day + "/" + month, docs[i].steps];
         data.push(punto);
       }
       var salida = {"label": "Actividad semanal", "data": data};
