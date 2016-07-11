51b05925a532f301750787bbf711457221d80488
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-07-11 13:53:41 +0100

    modificado el upload para utilizar la nueva vista

--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -42,7 +42,7 @@ exports.show = function (req, res){
 };
 
 exports.getUpload = function (req, res) {
-  res.render('pages/quiz/index', {title: 'Subir un nuevo test', partial: '../../partials/quiz/quizUpload', message: null, errors: []});
+  res.render('pages/quiz/upload', {title: 'Subir un nuevo test', message: null, errors: []});
 };
 
 exports.postUpload = function (req, res, next){
@@ -57,16 +57,16 @@ exports.postUpload = function (req, res, next){
           db.quizes.insert(obj, function (err) {
             if(err){
               console.log('Error: No se ha podido guardar el documento en la bd');
-              res.render('pages/quiz/index', {title: 'Subir un nuevo test', partial: '../../partials/quiz/quizUpload', message: null, errors: err.errors});
+              res.render('pages/quiz/upload', {title: 'Subir un nuevo test', message: null, errors: err.errors});
             } else {
               console.log('Correct: El documento se ha almacenado correctamente en la bd');
-              res.render('pages/quiz/index', {title: 'Subir un nuevo test', partial: '../../partials/quiz/quizUpload', message: 'Documento almacenado y volcado en la BBDD',errors: []});
+              res.render('pages/quiz/upload', {title: 'Subir un nuevo test', message: 'Documento almacenado y volcado en la BBDD',errors: []});
 
             }
           });
         });
       } else {
-        res.render('pages/quiz/index', {title: 'Subir un nuevo test', partial: '../../partials/quiz/quizUpload', message: 'Debes elegir un archivo.json',errors: []});
+        res.render('pages/quiz/upload', {title: 'Subir un nuevo test', message: 'Debes elegir un archivo.json',errors: []});
       }
     }
   });
