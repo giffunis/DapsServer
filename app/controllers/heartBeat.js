33932d041e76b45177d7f8713fe2dbe9321540e6
José Saúl Giffuni Becerra <giffunis@gmail.com>
2016-07-12 18:52:50 +0100

    Ahora enseña los datos por día y hora

--- app/controllers/heartBeat.js
+++ app/controllers/heartBeat.js
@@ -17,7 +17,7 @@ exports.show = function(req,res){
       var data = [];
       var punto;
       for (var i = 0; i < docs.length; i++) {
-        punto = [docs[i].created_at.getMonth() + "/" + docs[i].created_at.getDay(), docs[i].pulso];
+        punto = [docs[i].created_at.getHours() + ":" + docs[i].created_at.getMinutes(), docs[i].pulso];
         data.push(punto);
       }
       var salida = {"label": "Frecuencia Cardiaca", "data": data};
